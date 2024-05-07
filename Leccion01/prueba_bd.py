d2aeee69d54b768ca4a9439b88cbe0f7e877dc68
_Arom <117594980+Ranksine@users.noreply.github.com>
2024-05-07 17:51:11 -0600

    Update prueba_bd.py

--- Leccion01/prueba_bd.py
+++ Leccion01/prueba_bd.py
@@ -15,9 +15,12 @@ conexion = psycopg2.connect(user='postgres', password='admin', host='127.0.0.1',
 try:
     with conexion:
         with conexion.cursor() as cursor:
-            sentencia = 'SELECT * FROM persona'
-            cursor.execute(sentencia)
-            registros = cursor.fetchall()
+            sentencia = 'SELECT * FROM persona WHERE id_persona = %s'  # %s es llamado placeholder o parametro posicional
+            id_persona = input('Porporciona el valor id_persona: ')
+            cursor.execute(sentencia, (id_persona,))
+            # registros = cursor.fetchall  # Para recuperar todos los registros de la consulta
+            registros = cursor.fetchone()  # Para recuperar un solo registro de la consulta
+
             print(registros)
 
     conexion.close()
@@ -25,4 +28,3 @@ except Exception as e:
     print(f'Ocurrio un error: {e}')
 finally:
     conexion.close()
-    
