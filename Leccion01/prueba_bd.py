45dec9dbfedc2d365449eb35341d4e5e9cc9e660
_Arom <117594980+Ranksine@users.noreply.github.com>
2024-05-07 20:05:16 -0600

    Eliminar un registro en la base de datos

--- Leccion01/prueba_bd.py
+++ Leccion01/prueba_bd.py
@@ -15,14 +15,12 @@ conexion = psycopg2.connect(user='postgres', password='admin', host='127.0.0.1',
 try:
     with conexion:
         with conexion.cursor() as cursor:
-            sentencia = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
-            valores = (
-                ('Juan', 'Perez', 'jperez@gmail.com', 1),
-                ('Ivonne', 'Gutierrez', 'igutierrez@gmail.com', 2),
-            )
-            cursor.executemany(sentencia, valores)
-            registros_actualizados = cursor.rowcount
-            print(f'Registros Actualizados: {registros_actualizados}')
+            sentencia = 'DELETE FROM persona WHERE id_persona = %s'
+            entrada = input('Proporciona el id_persona a eliminar: ')
+            valores = (entrada,)
+            cursor.execute(sentencia, valores)
+            registros_eliminados = cursor.rowcount
+            print(f'Registros Eliminados: {registros_eliminados}')
     conexion.close()
 except Exception as e:
     print(f'Ocurrio un error: {e}')
