dfa186d4f51f937221f9947181fb0db084ae6a65
_Arom <117594980+Ranksine@users.noreply.github.com>
2024-05-07 20:07:40 -0600

    Eliminar varios registros en la base de datos

--- Leccion01/prueba_bd.py
+++ Leccion01/prueba_bd.py
@@ -15,9 +15,9 @@ conexion = psycopg2.connect(user='postgres', password='admin', host='127.0.0.1',
 try:
     with conexion:
         with conexion.cursor() as cursor:
-            sentencia = 'DELETE FROM persona WHERE id_persona = %s'
-            entrada = input('Proporciona el id_persona a eliminar: ')
-            valores = (entrada,)
+            sentencia = 'DELETE FROM persona WHERE id_persona IN %s'
+            entrada = input('Proporciona los id_persona a eliminar (separados por coma): ')
+            valores = (tuple(entrada.split(',')),)
             cursor.execute(sentencia, valores)
             registros_eliminados = cursor.rowcount
             print(f'Registros Eliminados: {registros_eliminados}')
